749ba8dce82d4c844a7bd7e8a17ffa37d18095d9
housamhegazy <geohousam@gmail.com>
2023-03-08 03:03:05 +0300

    menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useMemo, useState } from "react";
-import { createTheme, CssBaseline, ThemeProvider, Button } from "@mui/material";
+import { createTheme, CssBaseline, ThemeProvider, Button, Menu, MenuItem } from "@mui/material";
 import getDesignTokens from "./styles/MuTheme";
-
+import { useRef } from "react";
 function App() {
+ 
   const localTheme = localStorage.getItem("localTheme");
   const [mode, setmode] = useState(
     localTheme === null ? "light" : localTheme === "light" ? "light" : "dark"
   );
+
   const theme = useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
   //dark mood function
   const darkmoodFunc = () => {
@@ -16,7 +18,10 @@ function App() {
     );
     setmode(theme.palette.mode === "light" ? "dark" : "light");
   };
-
+//menu usestate
+const inputElement =useRef(null)
+const [isOpen,setisOpen]=useState(false)
+ const closeMenu = () => { setisOpen(false) }
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
@@ -30,6 +35,36 @@ function App() {
         >
           toggle mode
         </Button>
+
+
+      <Button ref={inputElement} sx={{ml:"199px"}}
+        variant="contained"
+        onClick={() => { 
+          setisOpen(!isOpen)
+        }}
+        
+      >
+        Dashboard
+      </Button>
+      
+      <Menu
+        id="basic-menu"
+        anchorEl={inputElement.current}
+        open={isOpen}
+        onClose={() => { closeMenu() }}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={() => {closeMenu()  }}>Profile</MenuItem>
+        <MenuItem onClick={() => {closeMenu()  }}>My account</MenuItem>
+        <MenuItem onClick={() => {closeMenu()  }}>Logout</MenuItem>
+      </Menu>
+
+
+
+
+
       </div>
     </ThemeProvider>
   );
