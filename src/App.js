99c3fc799f774a44213335999018bb02c80bfe4a
housamhegazy <geohousam@gmail.com>
2023-03-09 01:37:21 +0300

    appbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { useMemo, useState } from "react";
-import { createTheme, CssBaseline, ThemeProvider, Button, Menu, MenuItem } from "@mui/material";
+import {
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+  Button,
+  Menu,
+  MenuItem,
+  AppBar,
+} from "@mui/material";
 import getDesignTokens from "./styles/MuTheme";
 import { useRef } from "react";
+import Appbar from "components/AppBar";
 function App() {
- 
   const localTheme = localStorage.getItem("localTheme");
   const [mode, setmode] = useState(
     localTheme === null ? "light" : localTheme === "light" ? "light" : "dark"
@@ -18,14 +26,11 @@ function App() {
     );
     setmode(theme.palette.mode === "light" ? "dark" : "light");
   };
-//menu usestate
-const inputElement =useRef(null)
-const [isOpen,setisOpen]=useState(false)
- const closeMenu = () => { setisOpen(false) }
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <div className="App">
+        <Appbar />
         <Button
           onClick={() => {
             darkmoodFunc();
@@ -35,36 +40,6 @@ const [isOpen,setisOpen]=useState(false)
         >
           toggle mode
         </Button>
-
-
-      <Button ref={inputElement} sx={{ml:"199px"}}
-        variant="contained"
-        onClick={() => { 
-          setisOpen(!isOpen)
-        }}
-        
-      >
-        Dashboard
-      </Button>
-      
-      <Menu
-        id="basic-menu"
-        anchorEl={inputElement.current}
-        open={isOpen}
-        onClose={() => { closeMenu() }}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-      >
-        <MenuItem onClick={() => {closeMenu()  }}>Profile</MenuItem>
-        <MenuItem onClick={() => {closeMenu()  }}>My account</MenuItem>
-        <MenuItem onClick={() => {closeMenu()  }}>Logout</MenuItem>
-      </Menu>
-
-
-
-
-
       </div>
     </ThemeProvider>
   );
